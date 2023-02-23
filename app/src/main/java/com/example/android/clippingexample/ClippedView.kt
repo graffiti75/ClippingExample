453640825bf205ee73dcc0183c9f5ad8aa538ff8
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-22 20:35:13 -0500

    Adding code from section "4.5.16. (0:55) Implement drawCircularClippingExample"

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -94,7 +94,7 @@ class ClippedView @JvmOverloads constructor(
 	private fun drawDifferenceClippingExample(canvas: Canvas) {
 		canvas.save()
 		// Move the origin to the right for the next rectangle.
-		canvas.translate(columnTwo,rowOne)
+		canvas.translate(columnTwo, rowOne)
 		// Use the subtraction of two clipping rectangles to create a frame.
 		canvas.clipRect(
 			2 * rectInset,2 * rectInset,
@@ -124,7 +124,28 @@ class ClippedView @JvmOverloads constructor(
 	}
 
 	private fun drawCircularClippingExample(canvas: Canvas) {
+		canvas.save()
+		canvas.translate(columnOne, rowTwo)
+		// Clears any lines and curves from the path but unlike reset(),
+		// keeps the internal data structure for faster reuse.
+		path.rewind()
+		path.addCircle(
+			circleRadius,clipRectBottom - circleRadius,
+			circleRadius,Path.Direction.CCW
+		)
+		// The method clipPath(path, Region.Op.DIFFERENCE) was deprecated in
+		// API level 26. The recommended alternative method is
+		// clipOutPath(Path), which is currently available in
+		// API level 26 and higher.
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+			canvas.clipPath(path, Region.Op.DIFFERENCE)
+		} else {
+			canvas.clipOutPath(path)
+		}
+		drawClippedRectangle(canvas)
+		canvas.restore()
 	}
+
 	private fun drawIntersectionClippingExample(canvas: Canvas) {
 	}
 	private fun drawCombinedClippingExample(canvas: Canvas) {
